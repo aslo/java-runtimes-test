07f6bf25e5c61c1336570279f11f3bd49b9a1a61
Alex Sloan <alexsloan@google.com>
2016-08-29 17:20:17 -0400

    Add push_image function

--- run.sh
+++ run.sh
@@ -8,6 +8,14 @@ files=`find $test_dir -executable -type f`
 gcloud info
 gcloud auth list
 
+. helpers.sh
+
+# build and push the runtimes under test
+# TODO clone and build
+push_image openjdk8:latest openjdk8
+push_image jetty9:latest jetty9
+
+# run all tests
 for file in $files; do
   . $file 
 done
