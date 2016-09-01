fef7514b3dc87c880d5f4473df5bb2774f049d74
Alex Sloan <alexsloan@google.com>
2016-09-01 12:29:31 -0400

    Refactor build and deploy process using maven profiles

--- test/debugger.sh
+++ test/debugger.sh
@@ -5,25 +5,19 @@
 set -e
 
 project_root=`dirname $0`/..
-fixtures_dir=$project_root/fixtures
-declare -a test_apps=($fixtures_dir/jar-app $fixtures_dir/servlet-app)
+sample_app="$project_root/fixtures/sample-app"
+declare -a runtimes=("openjdk" "jetty")
 
 source $project_root/helpers.sh
 
+# TODO grep this from deploy output
 project=`get_active_project`
 project_url_suffix="$(echo $project | tr _ -).appspot.com"
 project_version="debugger-test"
 deployed_service_url="https://$project_version-dot-$project_url_suffix"
 
-function deploy_and_verify() {
-  app_dir=$1
-
-  pushd $app_dir
-
-  # build and deploy
-  mvn clean appengine:stage
-  sed -i "s/GCP_PROJECT/${project}/g" target/appengine-staging/Dockerfile
-  gcloud app deploy target/appengine-staging/app.yaml --version=$project_version --no-promote --quiet
+for runtime in ${runtimes[@]}; do
+  deploy $runtime $sample_app $project_version
 
   # TODO clear existing breakpoints
 
@@ -37,11 +31,5 @@ function deploy_and_verify() {
   # verify that the breakpoint has been hit
   # TODO
   #gcloud beta debug snapshots list ...
-
-  popd
-}
-
-for app in "${test_apps[@]}"; do
-  deploy_and_verify $app
 done
 
